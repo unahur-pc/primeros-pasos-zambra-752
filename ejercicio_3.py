c020022a195458ebbc772d4ca7c992a24b61ba66
zambra-752 <142693264+zambra-752@users.noreply.github.com>
2023-08-30 03:49:32 +0000

    actualizar

---
+++ ejercicio_3.py
@@ -0,0 +1,12 @@
+num_1=int(input("Ingrese el primer numero") )
+num_2=int(input("Ingrese el segundo numero") )
+suma=num_1+num_2
+resta=num_1-num_2
+producto=num_1*num_2
+division=num_1//num_2
+resto=num_1%num_2
+print(f"La suma de {num_1} + {num_2}es igual a: {suma}")
+print(f"La resta de {num_1} - {num_2} es igual a: {resta}")
+print(f"La multiplicacion de {num_1} * {num_2} es igual a: {producto}")
+print(f"La division de {num_1}//{num_2} es igual a: {division}")
+print(f"El resto de la division de {num_1} % {num_2} es igual a: {resto}")
