aecb953cc8080fd26090ad2d37a64f32b93ee4da
zambra-752 <142693264+zambra-752@users.noreply.github.com>
2023-08-31 20:25:32 +0000

    actualizar

--- ejercicio_6.py
+++ ejercicio_6.py
@@ -1,7 +1,7 @@
 print("Quiere saber cuanto cobra!!")
 horas=int(input("Ingrese la cantidad de horas trabajadas: "))
 
-costo=int(input("Ingrese el valor de la hora de trabajo: $"))
+costo=float(input("Ingrese el valor de la hora de trabajo: $"))
 pago=costo*horas
 
 print("el sueldo que debes cobrar es de: $",pago)
