caf76dae75a53f278e14135ab75c44523fd05c0d
zambra-752 <142693264+zambra-752@users.noreply.github.com>
2023-08-31 20:20:27 +0000

    actualizar

---
+++ ejercicio_5.py
@@ -0,0 +1,6 @@
+print("Pantalla para transformar de grados celsius a fharenheit")
+
+celsius=float(input("Ingrese los grados Celsius: "))
+fahremheit= (celsius*(9/5))+32
+
+print("Los grados en fahrenheit son: ",fahremheit)
