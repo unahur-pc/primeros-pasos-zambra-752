4f0e24d7f61e6331d90b0be3cddc9eb74476053c
zambra-752 <142693264+zambra-752@users.noreply.github.com>
2023-09-02 23:34:39 +0000

    actualizar

---
+++ ejercicio_9.py
@@ -0,0 +1,7 @@
+num1=int(input("Ingrese el primer número: "))
+num2=int(input("Ingrese el segundo número: "))
+
+if num1==num2:
+    print("Los números son iguales")
+else:
+    print("Los números no son iguales")
