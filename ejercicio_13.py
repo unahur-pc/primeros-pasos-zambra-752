527afe5cd82e04be31c4c9a1295bc6fe1eaaa2dd
zambra-752 <142693264+zambra-752@users.noreply.github.com>
2023-09-07 02:45:26 +0000

    actualizar

---
+++ ejercicio_13.py
@@ -0,0 +1,10 @@
+# Calcular la suma de 10 números, utilizando la misma variable para leer los números
+# desde la terminal .
+vueltas=1 # variable que controla el bucle.
+suma=0
+print("Se van a sumar los 10 números ")
+while vueltas <=10 :
+      num=int(input("Ingrese un número "))
+      suma=suma+1
+      vueltas=vueltas+1 # actualiza
+print(f"la suma de los 10 números es: {suma}")
